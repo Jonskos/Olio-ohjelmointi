dd3233794c91a42864ca23fdfced3c9cc8d33281
Jonskus <Jonskumelvis@protonmail.com>
2024-03-26 18:04:03 +0200

    Add Harjoitus3 functions

--- Harjoitus3/Kiuas.cs
+++ Harjoitus3/Kiuas.cs
@@ -8,6 +8,41 @@ namespace Harjoitus3
 {
     internal class Kiuas
     {
-
+        private bool paalla;
+        public int lampotila;
+        public int kosteus;
+        public void Kaynnista()
+        {
+            paalla = true;
+        }
+        public void Sammuta()
+        {
+            paalla = false;
+        }
+        public bool Paalla()
+        {
+            return paalla;
+        }
+        public string Tila()
+        {
+            if (paalla == true)
+            {
+                return "Päällä";
+            }
+            else
+            {
+                return "Pois";
+            }
+        }
+        public void Kaanna()
+        {
+            paalla = !paalla;
+        }
+        public Kiuas(bool paalla, int lampotila, int kosteus)
+        {
+            this.paalla = paalla;
+            this.lampotila = lampotila;
+            this.kosteus = kosteus;
+        }
     }
 }
--- Harjoitus3/Program.cs
+++ Harjoitus3/Program.cs
@@ -3,6 +3,14 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Kiuas kiuas = new Kiuas(false, 20, 5);
+        Console.WriteLine(kiuas.Tila());
+        kiuas.Kaynnista();
+        Console.WriteLine(kiuas.Tila());
+        kiuas.Sammuta();
+        Console.WriteLine(kiuas.Tila());
+        kiuas.Kaanna();
+        Console.WriteLine(kiuas.Tila());
+
     }
 }
