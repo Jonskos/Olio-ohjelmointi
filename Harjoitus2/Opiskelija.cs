7d176f5e960bb2659d00e066b86b206152cd7319
Jonskus <Jonskumelvis@protonmail.com>
2024-03-26 14:32:13 +0200

    Complete Harjoitus2

--- Harjoitus2/Opiskelija.cs
+++ Harjoitus2/Opiskelija.cs
@@ -8,6 +8,25 @@ namespace Harjoitus2
 {
     internal class Opiskelija
     {
+        private string Nimi { get; set; }
+        private string OpiskelijaID { get; set; }
+        private int Opintopisteet { get; set; }
 
+        public void TulostaData()
+        {
+            Console.WriteLine("Nimi: " + Nimi);
+            Console.WriteLine("OpiskelijaID " + OpiskelijaID);
+            Console.WriteLine("Opintopisteet: " + Opintopisteet);
+        }
+        public void MuokkaaOpintopisteitä(int pisteMaara)
+        {
+            Opintopisteet = pisteMaara;
+        }
+        public Opiskelija(string _nimi, string _opiskelijaID, int _opintopisteet)
+        {
+            Nimi = _nimi;
+            OpiskelijaID = _opiskelijaID;
+            Opintopisteet = _opintopisteet;
+        }
     }
 }
--- Harjoitus2/Program.cs
+++ Harjoitus2/Program.cs
@@ -1,7 +1,20 @@
-﻿internal class Program
+﻿using Harjoitus2;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Opiskelija Joonas = new Opiskelija("JOonas", "32AB", 179);
+        Opiskelija Nikke = new Opiskelija("Nikke", "32AC", 1);
+        Opiskelija Olavi = new Opiskelija("Olavi", "32AD", 301);
+
+        Joonas.TulostaData();
+        Console.WriteLine();
+
+        Nikke.TulostaData();
+        Console.WriteLine();
+
+        Olavi.TulostaData();
+
     }
 }
