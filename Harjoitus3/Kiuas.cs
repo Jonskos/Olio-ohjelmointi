2a085d949ec5f456621399848c00164dcd877e11
Jonskus <Jonskumelvis@protonmail.com>
2024-03-26 14:35:55 +0200

    Create Harjoitus3

---
+++ Harjoitus3/Kiuas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus3
+{
+    internal class Kiuas
+    {
+
+    }
+}
---
+++ Harjoitus3/Program.cs
@@ -0,0 +1,8 @@
+﻿using Harjoitus3;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
