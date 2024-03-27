e536ce022cf0cfb73d34bd5332295a3dbe788a90
Jonskus <Jonskumelvis@protonmail.com>
2024-03-27 09:06:42 +0200

    Completed Harjoitus3

--- Harjoitus3/Program.cs
+++ Harjoitus3/Program.cs
@@ -6,11 +6,25 @@ internal class Program
         Kiuas kiuas = new Kiuas(false, 20, 5);
         Console.WriteLine(kiuas.Tila());
         kiuas.Kaynnista();
+        kiuas.lampotila = 40;
+        kiuas.kosteus = 50;
         Console.WriteLine(kiuas.Tila());
+        Console.WriteLine("Lämpötila: " + kiuas.lampotila);
+        Console.WriteLine("Kosteus: " + kiuas.kosteus + "%");
         kiuas.Sammuta();
+        kiuas.lampotila = 20;
+        kiuas.kosteus = 20;
         Console.WriteLine(kiuas.Tila());
+        Console.WriteLine("Lämpötila: " + kiuas.lampotila);
+        Console.WriteLine("Kosteus: " + kiuas.kosteus + "%");
+
         kiuas.Kaanna();
+        kiuas.lampotila = 40;
+        kiuas.kosteus = 60;
         Console.WriteLine(kiuas.Tila());
+        Console.WriteLine("Lämpötila: " + kiuas.lampotila);
+        Console.WriteLine("Kosteus: " + kiuas.kosteus + "%");
+
 
     }
 }
