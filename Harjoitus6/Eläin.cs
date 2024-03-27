1ce7ff60406f423b94a98a84aca8b9c817fee63a
Jonskus <Jonskumelvis@protonmail.com>
2024-03-27 12:16:11 +0200

    Alusta Harjoitus6

---
+++ Harjoitus6/Eläin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus6
+{
+    internal class Eläin
+    {
+        public string genus;
+        private int instanssit = 0;
+
+        public Eläin()
+        {
+            instanssit++;
+        }
+    }
+}
