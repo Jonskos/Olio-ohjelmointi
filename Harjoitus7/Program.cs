441e9213e694c41a4408d9f96802da6a10da2e06
Jonskus <Jonskumelvis@protonmail.com>
2024-03-27 12:38:43 +0200

    Alusta Harjoitus7

---
+++ Harjoitus7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Write("Mihin kerrokseen haluat mennä? ");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int value))
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("Syötä numero.");
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
