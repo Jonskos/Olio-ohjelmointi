dda68d3858750fbf85252d801e9b9487900c5de5
Jonskus <Jonskumelvis@protonmail.com>
2024-03-27 11:09:52 +0200

    Complete Harjoitus5

--- Harjoitus5/Laskin.cs
+++ Harjoitus5/Laskin.cs
@@ -8,5 +8,22 @@ namespace Harjoitus5
 {
     internal static class Laskin
     {
+        public static float Summa(float a, float b)
+        {
+            return a + b;
+        }
+        public static float Erotus(float a, float b)
+        {
+            return a - b;
+        }
+        public static float Kertolasku(float a, float b)
+        {
+            return a * b;
+        }
+        public static float Jako(float a, float b)
+        {
+            return a / b;
+        }
+
     }
 }
--- Harjoitus5/Program.cs
+++ Harjoitus5/Program.cs
@@ -1,7 +1,41 @@
-﻿internal class Program
+﻿using Harjoitus5;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        float a;
+
+        while (true)
+        {
+            Console.Write("Syötä numero: ");
+            string syote = Console.ReadLine();
+            if (float.TryParse(syote, out a))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Ei ole numero.");
+            }
+        }
+        float b;
+        while (true)
+        {
+            Console.Write("Syötä numero: ");
+            string syote = Console.ReadLine();
+            if (float.TryParse(syote, out b))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Ei ole numero.");
+            }
+        }
+        Console.WriteLine($"Numeroiden {a} ja {b} summa on " + Laskin.Summa(a, b));
+        Console.WriteLine($"Numeroiden {a} ja {b} erotus on " + Laskin.Erotus(a, b));
+        Console.WriteLine($"Numeroiden {a} ja {b}  on " + Laskin.Kertolasku(a, b));
+        Console.WriteLine($"Numeroiden {a} ja {b} on " + Laskin.Jako(a, b));
     }
 }
