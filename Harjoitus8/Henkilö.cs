e688054f8f961b24c6f2bafdd3bb357d49eff82b
Jonskus <Jonskumelvis@protonmail.com>
2024-03-28 13:05:45 +0200

    Alusta Harjoitus8

---
+++ Harjoitus8/Henkilö.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus8
+{
+    internal abstract class Henkilö
+    {
+        private string nimi;
+        private string työpäaikka;
+        private string palkkaKK;
+        private string 
+
+    }
+}
---
+++ Harjoitus8/Pomo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus8
+{
+    internal class Pomo : Henkilö
+    {
+        private string auto;
+        private int boonus;
+        private int pomoInstansseja;
+        public Pomo(string auto, int boonus)
+        {
+            pomoInstansseja++;
+            this.auto = auto;
+            this.boonus = boonus;
+            if (pomoInstansseja = 1)
+            {
+                
+            }
+        }
+    }
+}
