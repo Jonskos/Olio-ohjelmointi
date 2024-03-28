28d68cdf6b5f123109579dac5dd6fa6bc6e9a803
Jonskus <Jonskumelvis@protonmail.com>
2024-03-28 13:05:27 +0200

    Complete Harjoitus7

--- Harjoitus7/Hissi.cs
+++ Harjoitus7/Hissi.cs
@@ -8,6 +8,69 @@ namespace Harjoitus7
 {
     internal class Hissi
     {
+        private int kerros = 0;
+        private int Kerros
+        {
+            get { return kerros; }
+            set
+            {
+                if (value > 14)
+                {
+                    Console.WriteLine("Error: Kerros on liian korkea.");
+                }
+                else if (value < 0)
+                {
+                    Console.WriteLine("Error: kerros on liian matala");
+                }
+                else
+                {
+                    kerros = value;
+                }
+            }
+        }
+        public Hissi() { }
+        public void Siirra(int paateKerros)
+        {
+            //Kerros = paateKerros;
+            SiirtoAnimaatio(paateKerros);
+            Kerros = paateKerros;
+            Console.WriteLine("Kerros "+ kerros);
+            Console.WriteLine("Perillä.");
+            Thread.Sleep(100);
+        }
+        public void SiirtoAnimaatio(int paateKerros)
+        {
+            if (paateKerros > 14)
+            {
+                Console.WriteLine("Error: Kerros on liian korkea.");
+                return;
 
+            }
+            else if (paateKerros < 0)
+            {
+                Console.WriteLine("Error: kerros on liian matala");
+                return;
+            }
+            if (Kerros < paateKerros)
+            {
+                for (int i = Kerros; i < paateKerros; i++)
+                {
+                    Console.WriteLine("Kerros " + i);
+                    Thread.Sleep(1000);
+                }
+            }
+            else if (Kerros > 0)
+            {
+                for (int i = Kerros; i > paateKerros; i--)
+                {
+                    Console.WriteLine("Kerros " + i);
+                    Thread.Sleep(1000);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Olet jo siinä kerroksessa.");
+            }
+        }
     }
 }
--- Harjoitus7/Program.cs
+++ Harjoitus7/Program.cs
@@ -1,15 +1,18 @@
-﻿using System;
+﻿using Harjoitus7;
+using System;
 internal class Program
 {
     private static void Main(string[] args)
     {
+        Hissi hissi = new Hissi();
+        Console.WriteLine("Hissin maksimikerros on 14.");
         while (true)
         {
             Console.Write("Mihin kerrokseen haluat mennä? ");
             string input = Console.ReadLine();
             if (int.TryParse(input, out int value))
             {
-                
+                hissi.Siirra(value);
             }
             else
             {
