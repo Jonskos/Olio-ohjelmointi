65fc93dab7801b54df021a41211eeb3489f7e629
Jonskus <Jonskumelvis@protonmail.com>
2024-04-23 21:29:02 +0300

    Create Harjoitus9

---
+++ Harjoitus9/Program.cs
@@ -0,0 +1,8 @@
+﻿using Harjoitus9;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Harjoitus9/Radio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus9
+{
+    internal class Radio
+    {
+        private bool paalla;
+        private int aanenvoimakkuus;
+        private float taajuus;
+
+        private int Aanenvoimakkuus
+        {
+            get
+            { return aanenvoimakkuus; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Error: ");
+                }
+                else
+                {
+                    aanenvoimakkuus = value;
+                }
+            }
+        }
+    }
+}
