5361443644b324a682d70c0b588ebedf23d98579
Jonskus <Jonskumelvis@protonmail.com>
2024-05-18 11:23:21 +0300

    Comment Harjoitus12

---
+++ Harjoitus12/Kortti.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus12
+{
+    internal class Kortti
+    {
+        public string maa;
+        public int numero;
+        public Kortti(string maa, int numero)
+        {
+            this.maa = maa;
+            this.numero = numero;
+        }
+    }
+}
--- Harjoitus12/Korttipakka.cs
+++ Harjoitus12/Korttipakka.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Harjoitus12
 {
     internal class Korttipakka
@@ -17,33 +18,36 @@ namespace Harjoitus12
             "Hertta",
             "Ruutu"
         };
-        private Dictionary<int, string> korttipakka = new Dictionary<int, string>();
+        //käytän dictionarya, intin joka on käytännössä dictionaryn indexi (en käyttänyt listaa älä kysy miksi) ja kortti, joka on tietenkin kortti
+        private Dictionary<int, Kortti> korttipakka = new Dictionary<int, Kortti>();
         public Korttipakka()
         {
+            //katsotaan jos korttien määrä on jaettavissa maitten määrällä, joten kortit voidaan jakaa tasaisesti
             if (korttienMaara % maat.Count != 0)
             {
                 throw new Exception("Huono maiden tai korttien määrä");
             }
+            //jaetaan korttien maara maiden maaralla
             int korttiaMaassa = korttienMaara / maat.Count;
             for (int i = 0; i < maat.Count; i++)
             {
                 for (int j = 0; j < korttiaMaassa; j++)
                 {
-                    //Lisää kortit paikan mukaan ()
-                    korttipakka.Add(j + korttiaMaassa * i, maat[i]);
+                    //Lisää kortit ensiksi yhdestä  korttienMaaraan asti ja lisataan korttiin maa ja kortin numero 1 - korttiaMaassa
+                    korttipakka.Add((j + 1) + (korttiaMaassa * i), new Kortti(maat[i], j + 1));
                 }
             }
         }
         public void TulostaPakka()
         {
-            foreach(KeyValuePair<int, string> entry in korttipakka)
+            /*foreach(KeyValuePair<int, Kortti> entry in korttipakka)
             {
-                Console.WriteLine(entry.Key + entry.Value);
+                Console.WriteLine("{0}. kortti pakassa on {1} - {2}", entry.Key, entry.Value.maa, entry.Value.numero);
+            }*/
+            for(int i = 1;i < korttipakka.Count;i++)
+            {
+                Console.WriteLine("{0}. kortti pakassa on {1} - {2}", i, korttipakka[i].maa, korttipakka[i].numero);
             }
-        }
-        public void Shuffle()
-        {
-
         }
     }
 }
