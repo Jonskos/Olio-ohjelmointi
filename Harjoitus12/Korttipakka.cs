2eb1565066e8d073d8078f974ea5ae5d89685716
Jonskus <Jonskumelvis@protonmail.com>
2024-05-18 11:29:57 +0300

    Change dictionary to list

--- Harjoitus12/Korttipakka.cs
+++ Harjoitus12/Korttipakka.cs
@@ -9,6 +9,7 @@ namespace Harjoitus12
 {
     internal class Korttipakka
     {
+        //aloitusluvut
         private int korttienMaara = 52;
         private bool jokeritPaalla = false;
         public List<string> maat = new List<string>()
@@ -18,8 +19,8 @@ namespace Harjoitus12
             "Hertta",
             "Ruutu"
         };
-        //käytän dictionarya, intin joka on käytännössä dictionaryn indexi (en käyttänyt listaa älä kysy miksi) ja kortti, joka on tietenkin kortti
-        private Dictionary<int, Kortti> korttipakka = new Dictionary<int, Kortti>();
+        //käytän listaa jossa on kortit
+        private List<Kortti> korttipakka = new List<Kortti>();
         public Korttipakka()
         {
             //katsotaan jos korttien määrä on jaettavissa maitten määrällä, joten kortit voidaan jakaa tasaisesti
@@ -31,19 +32,15 @@ namespace Harjoitus12
             int korttiaMaassa = korttienMaara / maat.Count;
             for (int i = 0; i < maat.Count; i++)
             {
-                for (int j = 0; j < korttiaMaassa; j++)
+                for (int j = 0; j < korttiaMaassa + 1; j++)
                 {
-                    //Lisää kortit ensiksi yhdestä  korttienMaaraan asti ja lisataan korttiin maa ja kortin numero 1 - korttiaMaassa
-                    korttipakka.Add((j + 1) + (korttiaMaassa * i), new Kortti(maat[i], j + 1));
+                    //Lisään Kortin ja siihen maan ja numeron maassa.
+                    korttipakka.Add(new Kortti(maat[i], j));
                 }
             }
         }
         public void TulostaPakka()
         {
-            /*foreach(KeyValuePair<int, Kortti> entry in korttipakka)
-            {
-                Console.WriteLine("{0}. kortti pakassa on {1} - {2}", entry.Key, entry.Value.maa, entry.Value.numero);
-            }*/
             for(int i = 1;i < korttipakka.Count;i++)
             {
                 Console.WriteLine("{0}. kortti pakassa on {1} - {2}", i, korttipakka[i].maa, korttipakka[i].numero);
