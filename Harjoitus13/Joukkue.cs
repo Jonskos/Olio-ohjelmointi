0e261083f4118d28f27dfb17434cbcc5a05a191d
Jonskus <Jonskumelvis@protonmail.com>
2024-05-18 11:46:08 +0300

    Create Harjoitus 13

---
+++ Harjoitus13/Joukkue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus13
+{
+    internal class Joukkue
+    {
+        private string Nimi;
+        private string Kotikaupunki;
+        private Dictionary<int, Pelaaja> Pelaajat = new Dictionary<int, Pelaaja>();
+        public Pelaaja HaePelaaja(int numero)
+        {
+            return null;
+        }
+        public void LisääPelaaja(Pelaaja pelaaja)
+        {
+
+        }
+        public void PoistaPelaaja(int numero)
+        {
+
+        }
+        public List<Pelaaja> HaePelaajat()
+        {
+            return null;
+        }
+    }
+}
---
+++ Harjoitus13/Pelaaja.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus13
+{
+    internal class Pelaaja
+    {
+        private string Etunimi;
+        private string Sukunimi;
+        private int PelaajaNumero;
+
+    }
+}
