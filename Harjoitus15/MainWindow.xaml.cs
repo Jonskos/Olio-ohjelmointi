8462af61fb4e2194a490fdd553e800b4879a852b
Jonskus <Jonskumelvis@protonmail.com>
2024-05-20 08:47:38 +0300

    Complete Harjoitus15

---
+++ Harjoitus15/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Harjoitus15
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public int kuormaautotCounter = 0;
+        public int henkiloautotCounter = 0;
+
+        private void henkilöautoNappi_OnClick(object sender, RoutedEventArgs e)
+        {
+            henkiloautotCounter++;
+            henkilöautoText.Text = $"henkilöautoja: {henkiloautotCounter}";
+        }
+
+        private void kuormaautoNappi_OnClick(object sender, RoutedEventArgs e)
+        {
+            kuormaautotCounter++;
+            kuormaautoText.Text = $"kuormaautoja: {kuormaautotCounter}";
+        }
+    }
+}
