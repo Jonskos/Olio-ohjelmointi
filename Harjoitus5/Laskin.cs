5550480b2aec5add71b45d5067250d024218bd9b
Jonskus <Jonskumelvis@protonmail.com>
2024-05-23 15:14:25 +0300

    Create Harjoitus18

--- Harjoitus5/Laskin.cs
+++ Harjoitus5/Laskin.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Harjoitus5
 {
-    internal static class Laskin
+    public static class Laskin
     {
         public static float Summa(float a, float b)
         {
