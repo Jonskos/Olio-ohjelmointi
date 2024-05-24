0fe8e6573668991dc202fd89bfd41cec5ee0328b
Jonskus <Jonskumelvis@protonmail.com>
2024-05-24 11:35:06 +0300

    Complete harjoitus18

--- Harjoitus18/MainWindow.xaml.cs
+++ Harjoitus18/MainWindow.xaml.cs
@@ -21,5 +21,40 @@ namespace Harjoitus18
         {
             InitializeComponent();
         }
+
+        private void Laskin_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)e.OriginalSource;
+            inputTb.Text = inputTb.Text.ToString() + button.Content;
+        }
+        private void Lasku_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)e.OriginalSource;
+            
+            if (!float.TryParse(inputTb.Text, out float input1))
+            {
+                throw new Exception();
+            }
+            if(!float.TryParse(inputTb2.Text, out float input2))
+            {
+                throw new Exception();
+            }
+            switch (button.Content)
+            {
+                case "Summa":
+                    outputTb.Text = Harjoitus5.Laskin.Summa(input1, input2).ToString();
+                    break;
+                case "Erotus":
+                    outputTb.Text = Harjoitus5.Laskin.Erotus(input1, input2).ToString();
+                    break;
+                case "Jako":
+                    outputTb.Text = Harjoitus5.Laskin.Jako(input1, input2).ToString();
+                    break;
+                case "Kertolasku":
+                    outputTb.Text = Harjoitus5.Laskin.Kertolasku(input1, input2).ToString();
+                    break;
+            }
+        }
+
     }
 }
