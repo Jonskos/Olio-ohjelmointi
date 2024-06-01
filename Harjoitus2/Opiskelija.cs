755926df2bb2e3a75b92dfd5adb4b8e81902abb2
Jonskus <Jonskumelvis@protonmail.com>
2024-06-01 14:47:52 +0300

    Comment Harjoitus2

--- Harjoitus2/Opiskelija.cs
+++ Harjoitus2/Opiskelija.cs
@@ -8,20 +8,24 @@ namespace Harjoitus2
 {
     internal class Opiskelija
     {
+        //get; set; joten voi asettaa muuttujan ulkopuolisesti.
         private string Nimi { get; set; }
         private string OpiskelijaID { get; set; }
         private int Opintopisteet { get; set; }
 
+        //tulostaa opiskelijan eri ominaisuudet
         public void TulostaData()
         {
             Console.WriteLine("Nimi: " + Nimi);
             Console.WriteLine("OpiskelijaID " + OpiskelijaID);
             Console.WriteLine("Opintopisteet: " + Opintopisteet);
         }
+        //Muuntaa oppilaan opintopisteet annetuiksi parametreiksi
         public void MuokkaaOpintopisteitä(int pisteMaara)
         {
             Opintopisteet = pisteMaara;
         }
+        //Opiskelijan konstruktori
         public Opiskelija(string _nimi, string _opiskelijaID, int _opintopisteet)
         {
             Nimi = _nimi;
--- Harjoitus2/Program.cs
+++ Harjoitus2/Program.cs
@@ -4,16 +4,20 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        //Tekee kolme eri opiskelijaa
         Opiskelija Joonas = new Opiskelija("JOonas", "32AB", 179);
         Opiskelija Nikke = new Opiskelija("Nikke", "32AC", 1);
         Opiskelija Olavi = new Opiskelija("Olavi", "32AD", 301);
 
+        //Tulostaa "Joonas" opiskelijan datan
         Joonas.TulostaData();
         Console.WriteLine();
 
+        //Tulostaa "Nikke" opiskelijan datan
         Nikke.TulostaData();
         Console.WriteLine();
 
+        //Tulostaa "Olavi" opiskelijan datan
         Olavi.TulostaData();
 
     }
