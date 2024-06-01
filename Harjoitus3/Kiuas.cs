07c2f3c1c044ed22a34be11af07c0b4bfe0d54ff
Jonskus <Jonskumelvis@protonmail.com>
2024-06-01 15:00:29 +0300

    Comment Harjoitus3

--- Harjoitus3/Kiuas.cs
+++ Harjoitus3/Kiuas.cs
@@ -11,18 +11,22 @@ namespace Harjoitus3
         private bool paalla;
         public int lampotila;
         public int kosteus;
+        //Käynnistää kiuaan
         public void Kaynnista()
         {
             paalla = true;
         }
+        //Sammuttaa kiuaan
         public void Sammuta()
         {
             paalla = false;
         }
+        //Palauttaa muuttujan "paalla" tilan
         public bool Paalla()
         {
             return paalla;
         }
+        //Katsoo jos kiuas on päällä tai pois, ja palauttaa vastauksen
         public string Tila()
         {
             if (paalla == true)
@@ -34,10 +38,12 @@ namespace Harjoitus3
                 return "Pois";
             }
         }
+        //Vaihtaa kiukaan tilan päälle jos ei ole ja pois jos on
         public void Kaanna()
         {
             paalla = !paalla;
         }
+        //Kiukaan constructori
         public Kiuas(bool paalla, int lampotila, int kosteus)
         {
             this.paalla = paalla;
--- Harjoitus3/Program.cs
+++ Harjoitus3/Program.cs
@@ -3,24 +3,32 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        //instantioi uuden kiukaan
         Kiuas kiuas = new Kiuas(false, 20, 5);
+        //tulostaa kiukaan tilan
         Console.WriteLine(kiuas.Tila());
+        //käynnistää kiukaan
         kiuas.Kaynnista();
+        //Asettaa kiukaan lämpötila ja kosteus.
         kiuas.lampotila = 40;
         kiuas.kosteus = 50;
+        //tulostaa kiukaan tilan, lämpötilan ja kosteuden
         Console.WriteLine(kiuas.Tila());
         Console.WriteLine("Lämpötila: " + kiuas.lampotila);
         Console.WriteLine("Kosteus: " + kiuas.kosteus + "%");
+        //Sammuttaa kiukaan, ja asettaa lämpötilan ja kosteuden alemmaksi
         kiuas.Sammuta();
         kiuas.lampotila = 20;
         kiuas.kosteus = 20;
+        //tulostaa kiukaan tilan, lämpötilan ja kosteuden
         Console.WriteLine(kiuas.Tila());
         Console.WriteLine("Lämpötila: " + kiuas.lampotila);
         Console.WriteLine("Kosteus: " + kiuas.kosteus + "%");
-
+        //Kääntää kiukaan kahvan (eli muuttaa kiukaan tilan toiseksi kun se oli ennen)
         kiuas.Kaanna();
         kiuas.lampotila = 40;
         kiuas.kosteus = 60;
+        //tulostaa kiukaan tilan, lämpötilan ja kosteuden
         Console.WriteLine(kiuas.Tila());
         Console.WriteLine("Lämpötila: " + kiuas.lampotila);
         Console.WriteLine("Kosteus: " + kiuas.kosteus + "%");
